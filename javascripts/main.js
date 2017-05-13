142355917cf4f22bd73a10ecb6358567d9db2315
drworm75 <dwgames@gmail.com>
2017-05-13 11:43:09 -0500

    To Do list printing to dom from firebase

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,6 @@
-var app = angular.module("TodoApp", []);
-
+app.run((FIREBASE_CONFIG) => {
+  firebase.initializeApp(FIREBASE_CONFIG);
+});
 
 app.controller("NavCtrl", ($scope) => {
 
@@ -7,29 +8,10 @@ app.controller("NavCtrl", ($scope) => {
 	$scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Items"}];
 });
 
-app.controller("ItemCtrl", ($scope) => {
+app.controller("ItemCtrl", ($http, $q, $scope, FIREBASE_CONFIG) => {
 	$scope.dog = "Woof!";
 	$scope.showListView = true;
-	$scope.items = [
-        {
-          id: 0,
-          task: "mow the lawn",
-          isCompleted: true,
-          assignedTo: "Callan",
-        },
-        {
-          id: 1,
-          task: "grade quizzes",
-          isCompleted: false,
-          assignedTo: "Lauren",
-        },
-        {
-          id: 2,
-          task: "take a nap",
-          isCompleted: false,
-          assignedTo: "Zoe",
-        }
-	];
+	$scope.items = [];
 
 	$scope.newItem = () => {
 		$scope.showListView = false;
@@ -40,4 +22,32 @@ app.controller("ItemCtrl", ($scope) => {
 		console.log("all items");
 		$scope.showListView = true;
 	};	
+
+  let getItemList = () => {
+    let itemz = [];
+    return $q((resolve, reject) => {
+      $http.get(`${FIREBASE_CONFIG.databaseURL}/items.json`)
+      .then((fbItems) => {
+        var itemCollection = fbItems.data;
+        Object.keys(itemCollection).forEach((key) => {
+          itemCollection[key].id=key;
+          itemz.push(itemCollection[key]);
+          });
+          resolve(itemz);
+      })
+      .catch((error) => {
+        reject(error);
+      });
+    });
+  };
+
+  let getItems = () => {
+    getItemList().then((itemz) => {
+      $scope.items = itemz;
+    }).catch((error) => {
+      console.log("get Error", error);
+    });
+  };
+
+  getItems();
 });
