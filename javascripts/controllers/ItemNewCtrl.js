c21750d73399b694b922c34ae73756ac7652521a
drworm75 <dwgames@gmail.com>
2017-05-16 18:50:34 -0500

    Zoe told us to push

--- javascripts/controllers/ItemNewCtrl.js
+++ javascripts/controllers/ItemNewCtrl.js
@@ -1,9 +1,10 @@
-app.controller("ItemNewCtrl", function($scope, ItemFactory)  {
+app.controller("ItemNewCtrl", function($location, $scope, ItemFactory)  {
 
   $scope.addNewItem = () => {
     $scope.newTask.isCompleted = false;
     ItemFactory.postNewItem($scope.newTask).then(() => {
       $scope.newTask = {};
+      $location.url("/items/list")
       //switch views
     }).catch((error) => {
       console.log("Add error", error);
