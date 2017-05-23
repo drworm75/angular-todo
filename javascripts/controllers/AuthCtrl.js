d41640d6ae55eb25baf724bf265ee8e99cc09401
drworm75 <dwgames@gmail.com>
2017-05-22 20:30:03 -0500

    prevent access to routes if user is not authenicated

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -2,7 +2,8 @@ app.controller("AuthCtrl", function($location, $rootScope, $scope, AuthFactory, 
     
     $scope.auth = {
         email: "a@a.com",
-        password: "aaaaaa"
+        password: "aaaaaa",
+        username: "a"
     };
 
     if($location.path() === "/logout") {
